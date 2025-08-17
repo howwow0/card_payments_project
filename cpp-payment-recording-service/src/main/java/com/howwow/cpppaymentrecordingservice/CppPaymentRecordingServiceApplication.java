ae95dbd89b2092f236ab5df0e9dedd4f5e68490a
HowWow <yarohsyk10@gmail.com>
2025-08-17 22:05:09 +0400

    Изменены названия микросервисов

---
+++ cpp-payment-recording-service/src/main/java/com/howwow/cpppaymentrecordingservice/CppPaymentRecordingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.howwow.cpppaymentrecordingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CppPaymentRecordingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CppPaymentRecordingServiceApplication.class, args);
+	}
+
+}
---
+++ cpp-payment-recording-service/src/test/java/com/howwow/cpppaymentrecordingservice/CppPaymentRecordingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howwow.cpppaymentrecordingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CppPaymentRecordingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
